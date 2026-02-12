b056330e315f6a71c1470184c5ecaa3afde90247
stephen <95516724+ArkansasIo@users.noreply.github.com>
2026-02-12 23:58:06 +0000

    Remove legacy window classes; unify all UI windows under Unity system

--- Assets/GoonzuUI/Scripts/Windows/OptionsWindow.cs
+++ Assets/GoonzuUI/Scripts/Windows/OptionsWindow.cs
@@ -1,37 +1,3 @@
-using System;
-
-namespace GoonzuGame.GUI
-{
-    public class OptionsWindow : UIWindow
-    {
-        public Dictionary<string, string> Settings { get; set; }
-        public OptionsWindow()
-        {
-            Settings = new Dictionary<string, string>();
-        }
-        public override void Show()
-        {
-            Console.WriteLine("Showing OptionsWindow");
-            DisplaySettings();
-        }
-        public void DisplaySettings()
-        {
-            Console.WriteLine("Settings:");
-            foreach (var kvp in Settings)
-                Console.WriteLine($"- {kvp.Key}: {kvp.Value}");
-        }
-        public void SetOption(string key, string value)
-        {
-            Settings[key] = value;
-            Console.WriteLine($"Set option: {key} = {value}");
-        }
-        public void RemoveOption(string key)
-        {
-            Settings.Remove(key);
-            Console.WriteLine($"Removed option: {key}");
-        }
-    }
-}
 using UnityEngine;
 using GoonzuUI.Windows;
 
